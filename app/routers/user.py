4c65d784975b7fe4e4e32685b7cc920408334b3d
VladVasili4 <dipol777@ya.ru>
2024-10-22 17:53:45 +0300

    Add files via upload

--- app/routers/user.py
+++ app/routers/user.py
@@ -16,13 +16,12 @@ async def all_users(db: Annotated[Session, Depends(get_db)]):
     users_all = db.scalars(select(User)).all()
     return users_all
 
-@router.get('/user_id')
+@router.get('/{user_id}')
 async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
-    try:
-        user_with_id = db.scalars(select(User).where(User.id == user_id))
+    user_with_id = db.scalars(select(User).where(User.id == user_id))
+    if user_with_id is not None:
         return user_with_id
-    except IndexError:
-        raise HTTPException(status_code=404, detail='Message Not found')
+    raise HTTPException(status_code=404, detail='User was not found')
 
 @router.post('/create')        # Здесь надо еще проверить на наличие такого юзера в базе. Вдруг он уже есть.
 async def create_user(db: Annotated[Session, Depends(get_db)], create_user_m: CreateUser):
@@ -37,10 +36,26 @@ async def create_user(db: Annotated[Session, Depends(get_db)], create_user_m: Cr
 
 
 @router.put('/update')
-async def update_user(db: Annotated[Session, Depends(get_db)]):
-    pass
+async def update_user(db: Annotated[Session, Depends(get_db)], user_id: int, up_user: UpdateUser ):
+    user_with_id = db.scalars(select(User).where(User.id == user_id))
+    if user_with_id is not None:
+        db.execute(update(User).where(User.id == user_id).values(firstname=up_user.firstname,
+                                                                  lastname=up_user.lastname,
+                                                                  age=up_user.age))
+        db.commit()
+        return {'status_code': status.HTTP_200_OK, 'transaction': 'User update is successful!'}
+    raise HTTPException(status_code=404, detail='User was not found')
+
+
 
 
 @router.delete('/delete')
-async def delete_user(db: Annotated[Session, Depends(get_db)]):
-    pass
+async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
+    user_del = db.scalars(select(User).where(User.id == user_id))
+    if user_del is not None:
+        db.execute(delete(User).where(User.id == user_id))
+        db.commit()
+        return {'status_code': status.HTTP_200_OK, 'transaction': 'User was deleted successful!'}
+    raise HTTPException(status_code=404, detail='User was not found')
+
+# uvicorn main:app
