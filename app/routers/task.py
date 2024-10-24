bea5d79116d83c9613c4d6484fde359720e389d0
VladVasili4 <dipol777@ya.ru>
2024-10-24 15:43:32 +0300

    Add files via upload

--- app/routers/task.py
+++ app/routers/task.py
@@ -1,27 +1,70 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from backend.db_depends import get_db
+from typing import Annotated
+from models import Task, User
+from sqlalchemy import insert, select, delete, update
+from schemas import CreateTask, UpdateTask
+from slugify import slugify
 
 router = APIRouter(prefix='/task', tags=['task'])
 
 
 @router.get('/')
-async def all_tasks():
-    pass
+async def all_tasks(db: Annotated[Session, Depends(get_db)]):
+    task_all = db.scalars(select(Task)).all()
+    return task_all
 
-@router.get('/task_id')
-async def task_by_id():
-    pass
+@router.get('/{task_id}')
+async def task_by_id(db: Annotated[Session, Depends(get_db)], task_id: int):
+    task_with_id = db.scalars(select(Task).where(Task.id == task_id)).first()
+    if task_with_id is None:
+        raise HTTPException(status_code=404, detail='Task was not found')
+
+    return task_with_id
+
+
 @router.post('/create')
-async def create_task():
-    pass
+async def create_task(db: Annotated[Session, Depends(get_db)], user_id: int, create_task_m: CreateTask):
+    user_with_id = db.scalars(select(User).where(User.id == user_id)).first()
+    if user_with_id is None:
+        raise HTTPException(status_code=404, detail='User not found')
+
+    db.execute(insert(Task).values(title=create_task_m.title,
+                                   content=create_task_m.content,
+                                   priority=create_task_m.priority,
+                                   slug=slugify(create_task_m.title),
+                                   user_id=user_id))
+    db.commit()
+    return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Task created successfully!'}
+
 
 
 @router.put('/update')
-async def update_task():
-    pass
+async def update_task(db: Annotated[Session, Depends(get_db)], task_id: int, up_task: UpdateTask ):
+    task_up = db.scalars(select(Task).where(Task.id == task_id)).first()
+    if task_up is None:
+        raise HTTPException(status_code=404, detail='Task was not found')
+
+    db.execute(update(Task).where(Task.id == task_id).values(title=up_task.title,
+                                                             content=up_task.content,
+                                                             priority=up_task.priority))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'Task update is successful!'}
 
 
 @router.delete('/delete')
-async def delete_task():
-    pass
+async def delete_task(db: Annotated[Session, Depends(get_db)], task_id: int):
+    task_del = db.scalars(select(Task).where(Task.id == task_id)).first()
+    if task_del is None:
+        raise HTTPException(status_code=404, detail='Task not found')
+
+    db.execute(delete(Task).where(Task.id == task_id))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'Task was deleted successful!'}
+
+
+
+
+
+# uvicorn main:app
--- app/routers/user.py
+++ app/routers/user.py
@@ -2,10 +2,9 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from backend.db_depends import get_db
 from typing import Annotated
-from models import User
+from models import User, Task
 from sqlalchemy import insert, select, delete, update
 from schemas import CreateUser, UpdateUser
-
 from slugify import slugify
 
 router = APIRouter(prefix='/user', tags=['user'])
@@ -18,18 +17,34 @@ async def all_users(db: Annotated[Session, Depends(get_db)]):
 
 @router.get('/{user_id}')
 async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
-    user_with_id = db.scalars(select(User).where(User.id == user_id))
-    if user_with_id is not None:
-        return user_with_id
-    raise HTTPException(status_code=404, detail='User was not found')
+    user_with_id = db.scalars(select(User).where(User.id == user_id)).first()
+    if user_with_id is None:
+        raise HTTPException(status_code=404, detail='User was not found')
+    return user_with_id
 
-@router.post('/create')        # Здесь надо еще проверить на наличие такого юзера в базе. Вдруг он уже есть.
-async def create_user(db: Annotated[Session, Depends(get_db)], create_user_m: CreateUser):
-    db.execute(insert(User).values(username=create_user_m.username,
-                                   firstname=create_user_m.firstname,
-                                   lastname=create_user_m.lastname,
-                                   age=create_user_m.age,
-                                   slug=slugify(create_user_m.username)))
+
+@router.get ("/user_id/tasks")
+async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    user_with_id = db.scalars(select(User).where(User.id == user_id)).first()
+    if user_with_id is None:
+        raise HTTPException(status_code=404, detail='User was not found')
+
+    db.scalars(select(Task).where(Task.user_id == user_id))
+    return {"user_id": user_with_id.id, "tasks": [task for task in user_with_id.tasks]}
+
+
+
+@router.post('/create')
+async def create_user(db: Annotated[Session, Depends(get_db)], cre_user: CreateUser):
+    exist_user = db.execute(select(User).where(User.username == cre_user.username)).first()
+    if exist_user:
+        raise HTTPException(status_code=400, detail="User with this username already exists")
+
+    db.execute(insert(User).values(username=cre_user.username,
+                                   firstname=cre_user.firstname,
+                                   lastname=cre_user.lastname,
+                                   age=cre_user.age,
+                                   slug=slugify(cre_user.username)))
     db.commit()
     return {'status_code': status.HTTP_201_CREATED, 'transaction': 'Successful'}
 
@@ -37,25 +52,32 @@ async def create_user(db: Annotated[Session, Depends(get_db)], create_user_m: Cr
 
 @router.put('/update')
 async def update_user(db: Annotated[Session, Depends(get_db)], user_id: int, up_user: UpdateUser ):
-    user_with_id = db.scalars(select(User).where(User.id == user_id))
-    if user_with_id is not None:
-        db.execute(update(User).where(User.id == user_id).values(firstname=up_user.firstname,
-                                                                  lastname=up_user.lastname,
-                                                                  age=up_user.age))
-        db.commit()
-        return {'status_code': status.HTTP_200_OK, 'transaction': 'User update is successful!'}
-    raise HTTPException(status_code=404, detail='User was not found')
+    user_with_id = db.scalars(select(User).where(User.id == user_id)).first()
+    if user_with_id is None:
+        raise HTTPException(status_code=404, detail='User was not found')
+
+    db.execute(update(User).where(User.id == user_id).values(firstname=up_user.firstname,
+                                                              lastname=up_user.lastname,
+                                                              age=up_user.age))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User update is successful!'}
+
 
 
 
 
 @router.delete('/delete')
 async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
-    user_del = db.scalars(select(User).where(User.id == user_id))
-    if user_del is not None:
-        db.execute(delete(User).where(User.id == user_id))
-        db.commit()
-        return {'status_code': status.HTTP_200_OK, 'transaction': 'User was deleted successful!'}
-    raise HTTPException(status_code=404, detail='User was not found')
+    user_del = db.scalars(select(User).where(User.id == user_id)).first()
+    if user_del is None:
+        raise HTTPException(status_code=404, detail='User was not found')
+
+    db.execute(delete(Task).where(Task.user_id == user_id))
+    db.execute(delete(User).where(User.id == user_id))
+    db.commit()
+    return {'status_code': status.HTTP_200_OK, 'transaction': 'User was deleted successful!'}
+
 
 # uvicorn main:app
+
+
